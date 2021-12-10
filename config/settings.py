cb7e9d4b31018eb02378174ba669015b59c991f0
ramiboutas <ramiboutas@hotmail.com>
2021-12-10 22:06:27 +0100

    settings

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 PRODUCTION = str(os.environ.get('PRODUCTION')) == '1'
 
