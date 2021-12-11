92df9415a87c12d603f9f7e5f02f89f57b179fe4
ramiboutas <ramiboutas@hotmail.com>
2021-12-11 10:35:35 +0100

    static in prod

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # for production
 
 # Media files
 MEDIA_ROOT = BASE_DIR.joinpath('media')
